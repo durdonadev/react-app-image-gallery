c3d2f603abe6dd3eb74ed54b331c130f05cf6c94
Durdona Ibragimova <durdona.dev@gmail.com>
2023-12-17 02:49:20 -0500

    Create image gallery app in react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,119 @@
-import "./App.css";
+import React from "react";
+// import { ImageApi } from "./api";
 
-function App() {
-    return;
+import "./App.css";
+import { ImageApi } from "./api";
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            images: [],
+            loading: false,
+            page: 1,
+            zoomedInImage: {},
+            showImage: false
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            this.setState({
+                loading: true
+            });
+            const result = await ImageApi.fetchImages(this.state.page);
+
+            this.setState({
+                images: result,
+                loading: false
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    loadMore = async () => {
+        try {
+            this.setState({
+                loading: true
+            });
+            const nextPage = this.state.page + 1;
+            const result = await ImageApi.fetchImages(nextPage);
+
+            this.setState((prevState) => {
+                return {
+                    images: [...prevState.images, ...result],
+                    page: nextPage,
+                    loading: false
+                };
+            });
+        } catch (error) {
+            throw new Error(error);
+        }
+    };
+
+    zoomInImage = (image) => {
+        this.setState({
+            showImage: true,
+            zoomedInImage: image
+        });
+    };
+
+    hideImage = () => {
+        this.setState({
+            showImage: false,
+            zoomedInImage: {}
+        });
+    };
+
+    render() {
+        return (
+            <main>
+                <h1>Unsplash Like App</h1>
+                <section>
+                    <div className="gallery">
+                        {this.state.images.map((image) => {
+                            return (
+                                <figure>
+                                    <img
+                                        src={image.download_url}
+                                        alt={image.athor}
+                                        onClick={() => this.zoomInImage(image)}
+                                    />
+                                </figure>
+                            );
+                        })}
+                    </div>
+                    {this.state.images.length >= 1 ? (
+                        <div>
+                            <button
+                                className="load-more"
+                                onClick={this.loadMore}
+                                disabled={this.loading}
+                            >
+                                Load More
+                            </button>
+                        </div>
+                    ) : null}
+                </section>
+                {this.state.showImage && (
+                    <div className="overlay">
+                        <span
+                            className="overlay__close-icon"
+                            onClick={this.hideImage}
+                        >
+                            &#10005;
+                        </span>
+                        <img
+                            className="zoomed-in-image"
+                            src={this.state.zoomedInImage.download_url}
+                            alt={this.state.zoomedInImage.author}
+                        />
+                    </div>
+                )}
+            </main>
+        );
+    }
 }
 
 export default App;
